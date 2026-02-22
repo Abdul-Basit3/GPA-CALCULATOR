21ef3301374fd6c9b4dd4a4926a78fd669cb7f26
Abdul Code <abdulbasitadam949@gmail.com>
2026-02-22 08:28:27 +0000

    Fix SpeedInsights import and improve dark mode visibility

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 /**
  * Application entry point
